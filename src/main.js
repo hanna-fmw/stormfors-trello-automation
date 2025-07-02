9c94f95f06f55f9b48504004a0cd3e8add3f190c
Hanna Hosk <112755425+hanna-fmw@users.noreply.github.com>
2025-07-02 22:05:50 +0200

    Updated text

--- src/main.js
+++ src/main.js
@@ -5,14 +5,10 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+  
+    <h1>Hello World</h1>
     <div class="card">
+    <p>This is a dummy app needed to generate a Trello API key</p>
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
