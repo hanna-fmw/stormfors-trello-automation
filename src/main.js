6d2f7d9fccb501750c6cbb170cf4a2905929dc19
Hanna Hosk <112755425+hanna-fmw@users.noreply.github.com>
2025-07-02 22:07:01 +0200

    Updated message

--- src/main.js
+++ src/main.js
@@ -6,14 +6,12 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
   
-    <h1>Hello World</h1>
+    <h3>Hello World</h3>
     <div class="card">
     <p>This is a dummy app needed to generate a Trello API key</p>
-      <button id="counter" type="button"></button>
+      
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+ 
   </div>
 `
 
